03e41e457b45fa9e0f993b7f77c14e277d3c9692
Alena Sharai <exultantislupus@gmail.com>
2023-10-19 11:35:44 +0400

    add pizza components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import { pizzaData } from './data';
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      {pizzaData.map((pizza) => (
+        <Pizza
+          key={pizza.name}
+          name={pizza.name}
+          ingridients={pizza.ingredients}
+          imgSrc={pizza.photoName}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Pizza({ imgSrc, name, ingridients }) {
+  return (
+    <div>
+      <img height='200px' src={imgSrc} alt='pizza' />
+      <h2>{name}</h2>
+      <p>{ingridients}</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
