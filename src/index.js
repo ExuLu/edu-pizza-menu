a62b148bf686338ea9af58abbb61a520cb8682df
Alena Sharai <exultantislupus@gmail.com>
2023-10-19 13:29:06 +0400

    add components and style them

--- src/index.js
+++ src/index.js
@@ -1,29 +1,67 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { pizzaData } from './data';
+import './index.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!</h1>
-      {pizzaData.map((pizza) => (
-        <Pizza
-          key={pizza.name}
-          name={pizza.name}
-          ingridients={pizza.ingredients}
-          imgSrc={pizza.photoName}
-        />
-      ))}
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza({ imgSrc, name, ingridients }) {
+function Header() {
   return (
-    <div>
-      <img height='200px' src={imgSrc} alt='pizza' />
-      <h2>{name}</h2>
-      <p>{ingridients}</p>
+    <header className='header'>
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+function Menu() {
+  return (
+    <main className='menu'>
+      <h2>Our Menu</h2>
+      <div className='pizzas'>
+        {pizzaData.map((pizza) => (
+          <Pizza
+            key={pizza.name}
+            name={pizza.name}
+            ingridients={pizza.ingredients}
+            imgSrc={pizza.photoName}
+            price={pizza.price}
+            soldOut={pizza.soldOut}
+          />
+        ))}
+      </div>
+    </main>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+
+  return (
+    <footer className='footer'>
+      {new Date().toLocaleTimeString()} We're currently{' '}
+      {isOpen ? 'open' : 'close'}
+    </footer>
+  );
+}
+
+function Pizza({ imgSrc, name, ingridients, price, soldOut }) {
+  return (
+    <div className={soldOut ? 'pizza sold-out' : 'pizza'}>
+      <img src={imgSrc} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingridients}</p>
+        <span>{price}</span>
+      </div>
     </div>
   );
 }
